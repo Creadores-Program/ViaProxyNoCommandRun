965f6c7efb2167003106c21dfe8ff2dbd27f6137
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-10 10:51:05 -0600

    Create Main.java

---
+++ src/main/java/org/CreadoresProgram/ViaProxyPterodactyl/Main.java
@@ -0,0 +1,22 @@
+package org.CreadoresProgram.ViaProxyPterodactyl;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.lang.reflect.Method;
+public class Main{
+  public static void main(String[] args){
+    try{
+      File file = new File(System.getProperty("user.dir") + "/ViaProxy.jar");
+      URL url = file.toURI().toURL();
+      URLClassLoader child = new URLClassLoader(new URL[] { url }, JarLoader.class.getClassLoader());
+      Class<?> classToLoad = Class.forName("net.raphimc.viaproxy.ViaProxy", true, child);
+      Method method = classToLoad.getDeclaredMethod("main", String[].class);
+      method.invoke(null, (Object) getArgsVP());
+    }catch(Exception ex){
+      ex.printStackTrace();
+    }
+  }
+  public static String[] getArgsVP(){
+    //if config or not
+  }
+}
